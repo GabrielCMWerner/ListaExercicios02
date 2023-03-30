2f40a1af2c1338b938057831f2ac089d5d9e7f84
GabrielCMWerner <gabrielcmw1094@gmail.com>
2023-03-30 12:45:08 -0300

    Add project files.

---
+++ Exercício03/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Exercício03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cifra de Cezar");
+
+            Console.WriteLine();
+
+            Console.Write("Digite a frase que será codificada: ");
+            string palavra = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine(); Console.WriteLine();
+
+            Console.Write("Digite o valor de 'X' para codificar a frase: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            char[] cifraSeparada = new char[palavra.Length];
+            char[] letrasSeparadas = new char[palavra.Length];
+
+            letrasSeparadas = palavra.ToCharArray();
+
+            for (int i = 0; i < letrasSeparadas.Length; i++)
+            {
+                int letrasint = letrasSeparadas[i];
+
+                int cifraint = letrasint + x;
+
+                cifraSeparada[i] = Convert.ToChar(cifraint);
+
+            }
+
+            Console.WriteLine();
+            string cifra = string.Join("", cifraSeparada);
+
+            Console.Write("A frase após ser codificada é: ");
+
+            Console.WriteLine(cifra);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ListaExercicios02/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Exercício01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma frase: \n");
+            string frase = Console.ReadLine();
+            Console.WriteLine();
+
+            string[] palavras = frase.Split(' ');
+            string[] palavrasTitleCase = new string[palavras.Length];
+
+            for (int i = 0; i < palavras.Length; i++)
+            {
+
+                string primeiraLetra = palavras[i].Substring(0, 1).ToUpper();
+                string restoDaPalavra = palavras[i].Substring(1);
+                palavrasTitleCase[i] = primeiraLetra + restoDaPalavra;
+            }
+
+            string novaFrase = string.Join(" ", palavrasTitleCase);
+
+            Console.WriteLine("Frase com as primeiras letras maiúsculas: \n");
+            Console.WriteLine(novaFrase);
+
+        }
+    }
+}
